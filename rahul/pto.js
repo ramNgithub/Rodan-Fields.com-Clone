4a8ad8b8fe18114eecf395952b2ea2cbfc18e263
Rahulzhp <akumari42872@gmail.com>
2022-08-26 20:23:38 +0530

    added aditional file

--- rahul/pto.js
+++ rahul/pto.js
@@ -17,13 +17,46 @@ function appen(Ano){
     let btn = document.createElement("button");
     btn.innerText = "ADD TO BAG";
     btn.addEventListener("click",function(){
-      addToCart(el);
+      addcart(Ano);
 
     });
     let price=document.createElement("h2")
-    price.innerText= " $"+ Ano.price
+    price.innerText= " $"+ Ano.price;
+    let de=document.createElement("span")
+    de.innerText="Best Seller"
+    
+    let more=document.createElement("div")
+    let day=document.createElement("h4")
+    day.innerText="60 Day Money Back Guarantee";
+    more.append(day);
 
     large.append(image);
-    imgo.append(name,p,size,price,btn)
+    imgo.append(name,p,size,price,btn,more)
 }
 appen(Ano)
+let cart=JSON.parse(localStorage.getItem("cart")) || [];
+function addcart(Ano){
+  
+  cart.push(Ano);
+  alert("product added to cart");
+  localStorage.setItem("cart",JSON.stringify(cart));
+  
+}
+
+let image1=document.getElementById("image1")
+let image2=document.getElementById("image2")
+let image3=document.getElementById("image3")
+
+function imgeadd(Ano){
+  let img1=document.createElement("img")
+  img1.src=Ano.image1;
+  let img2=document.createElement("img")
+  img2.src=Ano.image2;
+  let img3=document.createElement("img")
+  img3.src=Ano.image3;
+  image1.append(img1);
+  image2.append(img2);
+  image3.append(img3);
+
+}
+imgeadd(Ano)
