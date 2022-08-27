3b3cde62e9ec9c24e7b4db7dff8c24cfae93c883
Rahulzhp <akumari42872@gmail.com>
2022-08-27 14:43:12 +0530

    added all file

--- rahul/best.js
+++ rahul/best.js
@@ -4,6 +4,8 @@ let data=[
     title:"Helps clear and prevent adult acne while visibly minimizing pores + fine lines.",
     name:"UNBLEMISH Regimen",
     price: 199,
+    rat:"4.6(8228)",
+    star:"https://cdn-icons-png.flaticon.com/128/992/992001.png",
     image1:"https://www.rodanandfields.com/en-us/medias/UNWA125-Regimen-Explainer-Step1-570x570-Desktop.png?context=bWFzdGVyfGltYWdlc3w0Mjg3NHxpbWFnZS9wbmd8aW1hZ2VzL2gxNS9oZmIvMTM0MjgwMzAyMTAwNzgucG5nfDhkMWI4OTZmN2Y4YmE3NTQ4ZjJjZGEyN2ZkYjVhOWU4MTZkYTIwZjFhZmI3YzhmZGRjZWQyNDc0YmY4YThiMmE",
     image2:"https://www.rodanandfields.com/en-us/medias/UNTJ125-Regimen-Explainer-Step2-570x570-Desktop.png?context=bWFzdGVyfGltYWdlc3w0ODM1NnxpbWFnZS9wbmd8aW1hZ2VzL2hiNi9oNDIvMTM0MjgwMzEwNjIwNDYucG5nfGQ4YTdmOTk3NmZmMTgyZGQ5MTk5MmU5YzllNzBiZmNjNzBmOWJiODgwYzVhMzgyNDg0ODQ3NjY0MjA1YWMwZTE",
     image3:"https://www.rodanandfields.com/en-us/medias/UNTT030-Regimen-Explainer-Step3-570x570-Desktop.png?context=bWFzdGVyfGltYWdlc3w1Nzc0NXxpbWFnZS9wbmd8aW1hZ2VzL2hiMC9oN2YvMTM0MjgwMzE0NTUyNjIucG5nfDEyMGNkZjQ0MWI1Y2UzNjA1Y2UzNzcxMTE0NjU0YjJiMWQ0NWQ3NThkYzZmMTdjOGY3YWM0ZTFiNjBiZmNkODg",
@@ -13,6 +15,8 @@ let data=[
         title:"Applied nightly to promote the appearance of longer, stronger and darker-looking lashes. ",
         name:"R + F Lash Boost",
         price: 155,
+        rat:"3.9(3279)",
+        star:"https://cdn-icons-png.flaticon.com/128/992/992000.png",
         image1:"https://www.rodanandfields.com/en-us/medias/ENHLSH01-PDP-lifestyle-1168x1168-DESKTOP.jpg?context=bWFzdGVyfGltYWdlc3w3NTIxNnxpbWFnZS9qcGVnfGltYWdlcy9oYWQvaGQ2Lzg4MjE4NzgwNjMxMzQuanBnfDRlYzBjZmU2ZTg1MDMwMGY3NzYxMGNmMDNiODlkZTJjMGUwOTgxNzdlOGMxODBhNTlkZTNmN2IxMWMzYjhjODE",
         image2:"https://www.rodanandfields.com/en-us/medias/ENHLSH01-detail.jpg?context=bWFzdGVyfGltYWdlc3wzMjIzMHxpbWFnZS9qcGVnfGltYWdlcy9oMTMvaDYwLzg4MjE4NDk2ODYwNDYuanBnfDMzOTMxMWRkOGUyMWZiOTQzY2NhYzhlODM4NTFhZDcxZTU3Yzg0NGIyMDZmODE2N2M0YTBhZDUzZDg2MGEyNzI",
         image3:"https://www.rodanandfields.com/en-us/medias/ENHLSH01-Lash-Carousel-570x570.jpg?context=bWFzdGVyfGltYWdlc3w3MzczNXxpbWFnZS9qcGVnfGltYWdlcy9oMjQvaGNiLzE1Mzk1NDE0ODY3OTk4LmpwZ3wzYTRkNWZjM2UzN2RmYzE5ODdjMDFmYmQ3OWEzYWZmYzhhODYzNmExMzZjNDc4MmFkZDVkM2UxNjMwOWVhNjI1",
@@ -22,6 +26,8 @@ let data=[
         title:"Our most comprehensive anti-aging serum to revitalize and address all key signs of aging. ",
         name:"Total RF Serum",
         price: 175,
+        rat:"4.4(5528)",
+        star:"https://cdn-icons-png.flaticon.com/128/992/992001.png",
         image1:"https://www.rodanandfields.com/en-us/medias/TRFS-Lifestyle-Product-Global-570x570.jpg?context=bWFzdGVyfGltYWdlc3w1MTE3NnxpbWFnZS9qcGVnfGltYWdlcy9oZDYvaDdlLzEzNjYyODA0NzM4MDc4LmpwZ3w5NDA1Mzc5OGFjOTI1NzU1ZmMyZjlhY2Y0ZjdhNmI4NWU3NmVlMWIxYzY5OWMxNGRlMGIxZjExOWZkNmYyMGNk",
         image2:"https://www.rodanandfields.com/en-us/medias/ENFM030-Global-720x600-Desktop.jpg?context=bWFzdGVyfGltYWdlc3wzNzU0NHxpbWFnZS9qcGVnfGltYWdlcy9oMzQvaDQwLzEzNjkzOTY1NTk4NzUwLmpwZ3xlODg0YWM5YjBiZjM0M2I4MzMyYjE5NDMzMmZjMjg4ZjM1MDE1ZDg4YTI1NjFhNTJlNjg5ZWRjYWYzMTY5NDQ5",
         image3:"https://www.rodanandfields.com/en-us/medias/PS20-PopsugarPick-Seal-Navy-2020-160x160.png?context=bWFzdGVyfGltYWdlc3w5MjEyfGltYWdlL3BuZ3xpbWFnZXMvaDI5L2gwNC8xNDI5NTk0MTY3NzA4Ni5wbmd8NDNhNTkyMzRkYjcxNDI4YTI5MTU0ZmZkNzRhZTQ1NDY0YjJlZWFiMzNhNjA5NDMxMWJhYWU1MjIxNTY2ZDEzOQ",
@@ -31,6 +37,8 @@ let data=[
       title:"Instantly boosts hydration levels by 200% and locks in moisture on skin's surface. ",
       name:"R + F Active Hydration Serum",
       price: 112,
+      rat:"4.5(7859)",
+      star:"https://cdn-icons-png.flaticon.com/128/992/992001.png",
       image1:"https://www.rodanandfields.com/en-us/blog/wp-content/uploads/2020/05/RF2019_0670_hero_AHS_JPN_1100X1100-1024x1024.jpg",
       image2:"https://m.media-amazon.com/images/I/61YLKX70edL._SY679_.jpg",
       image3:"https://pictures.alignable.com/eyJidWNrZXQiOiJhbGlnbmFibGV3ZWItcHJvZHVjdGlvbiIsImtleSI6InNlcnZpY2VzL3BpY3R1cmVzL21lZGl1bS84MTA5NzYvMTUzNjI2Mzk3Nl9haHMxLmpwZyIsImVkaXRzIjp7fX0=",
@@ -40,6 +48,8 @@ let data=[
       title:"Protects sensitive skin, reduces visible redness + fortifies skin’s moisture barrier.",
       name:"SOOTHE Regimen",
       price: 185,
+      rat:"3.8(8228)",
+      star:"https://cdn-icons-png.flaticon.com/128/992/992000.png",
       image1:"https://www.rodanandfields.com/en-us/medias/SOTT050-Regimen-Explainer-Step2-570x570-Desktop.png?context=bWFzdGVyfGltYWdlc3wyNjQxNHxpbWFnZS9wbmd8aW1hZ2VzL2gyOS9oOTYvMTM0MjgwMjY4MDIyMDYucG5nfDRhMjNlMzg3NWUxNWEyYmRmZTc5MzRlMzQ2ZDY1OWQ1YTRmZmZiZTA3NTVmMThjOWY3NmUyM2UxNmI1ZWFiMTU",
       image2:"https://www.rodanandfields.com/en-us/medias/SOMO050-Regimen-Explainer-Step3-570x570-Desktop.png?context=bWFzdGVyfGltYWdlc3w1MzU5MXxpbWFnZS9wbmd8aW1hZ2VzL2gyMi9oZjkvMTM0MjgwMjc2NTQxNzQucG5nfGUzMTdjNzk3MzgyZmY1YWRlNjA3YWIzMmM4ZDZhYjhiYmY5N2IyZDRlNWZlYTZhNjE3ZDFmNzQ0ZjJjODIwMjE",
       image3:"https://www.rodanandfields.com/en-us/medias/SOOTHE-National-Eczema-Association-Seal-Desktop.png?context=bWFzdGVyfGltYWdlc3wxMTY2N3xpbWFnZS9wbmd8aW1hZ2VzL2g0MS9oZmEvMTQ4MDU5Nzc4MjUzMTAucG5nfDA1MTJjY2FiNGI1ZmU3MDQ5ZGNmMDc0Zjg5ZmY4NWQ1NzI0NDQyZWE4N2I3ZDkzM2EzNjM2Yzk2YzExNDE5ZTI",
@@ -49,6 +59,8 @@ let data=[
       title:"Reduces visible dark circles and under-eye puffiness over time for younger-looking eyes.",
       name:"Active Hydration Bright Eye",
       price: 75,
+      rat:"4.6(2229)",
+        star:"https://cdn-icons-png.flaticon.com/128/992/992001.png",
       image1:"https://www.tothineownstylebetrue.com/wp-content/uploads/2021/06/Rodan-Fields-REDEFINE-Eye-Cream-and-Bright-Eye-Complex-4.jpg",
       image2:"https://www.rodanandfields.com/en-us/medias/ENHEY15-detail.jpg?context=bWFzdGVyfGltYWdlc3wzNjA3NXxpbWFnZS9qcGVnfGltYWdlcy9oMjIvaGY2LzEzNTMzMTczNDgxNTAyLmpwZ3w0MzZiNDBmYmMyNGUwNjMyZjVhN2IyYThlZGM3YWE2ZWU0ZGZjYWRkNmFjYWM0YzMzMDc2NmMwN2M3YTI0MTlk",
       image3:"https://i.pinimg.com/originals/f2/20/96/f220962c2639ce9dd979a88dbee0db55.jpg",
@@ -58,6 +70,8 @@ let data=[
       title:"Instantly and continuously hydrates the body for younger-looking, younger-acting skin. ",
       name:"Rodan + Fields Active Hydration ",
       price: 66,
+      rat:"3.7(3279)",
+      star:"https://cdn-icons-png.flaticon.com/128/992/992000.png",
       image1:"https://www.rodanandfields.com/en-us/medias/ENBR001-PDP-lifestyle-1168x1168-DESKTOP.jpg?context=bWFzdGVyfGltYWdlc3w4ODU1OHxpbWFnZS9qcGVnfGltYWdlcy9oN2QvaGI4LzEzNTMzMTYzMzIzNDIyLmpwZ3xhZjg2ODI1Y2E1ZmM0YzM4MTFhOTllYjFmNmFiY2FjMGUyN2Q0NzkxYzcwMzRiNjhhYmExNDY1YzcyZmY0YzRm",
       image2:"https://www.rodanandfields.com/en-us/medias/ENBR001-detail.jpg?context=bWFzdGVyfHJvb3R8NDA3NjR8aW1hZ2UvanBlZ3xoNzIvaDdmLzEzNTMzMTYyNzk5MTM0LmpwZ3w5ZGQ5MDY2M2ZjM2U4M2NjZmI4OTRiMGU0OGI0YmZjNjRiNDUxM2RkYjNlZDY2MDEzZWU4YWQ4NTc2ZjRiNTA4",
       image3:"https://i.pinimg.com/originals/0f/43/da/0f43da78286387ad007a319fd23273fe.jpg",
@@ -67,6 +81,8 @@ let data=[
       title:"Brightens + refines skin’s surface while tackling the look of fine lines + dark spots. ",
       name:"REVERSE Regimen",
       price: 195,
+      rat:"4.8(7528)",
+        star:"https://cdn-icons-png.flaticon.com/128/992/992001.png",
       image1:"https://www.rodanandfields.com/en-us/medias/RVTG125-Regimen-Explainer-Step2-570x570-Desktop.png?context=bWFzdGVyfGltYWdlc3wzMjE1N3xpbWFnZS9wbmd8aW1hZ2VzL2g3Yy9oYTgvMTM0MjgwMjQ2NzIyODYucG5nfGMxZWI5NTk5ZjA0YTNlMWZiZmRlZGExMTQ3ZDBhYjkwN2Y1YWM1YjcyODE1YzU3Yzk4MjU4YTYyZTEwZGYyMmM",
       image2:"https://www.rodanandfields.com/en-us/medias/RVWA125-Regimen-Explainer-Step1-570x570-Desktop.png?context=bWFzdGVyfGltYWdlc3w0MjA3MnxpbWFnZS9wbmd8aW1hZ2VzL2g0Ny9oMzMvMTM0MjgwMjQ0MTAxNDIucG5nfDc1OTM3NDUzOTMyNWQxMzQ1YjhiM2NkMTlhMzk2OWU0MWZmMDhjZjQ1YmMxYjBhNTBhZjk0NzRkOTYwNjIyZjU",
       image3:"https://www.rodanandfields.com/en-us/medias/RVTTG50-Regimen-Explainer-Step3-AM-570x570-Desktop.png?context=bWFzdGVyfGltYWdlc3w2ODgzM3xpbWFnZS9wbmd8aW1hZ2VzL2hjZC9oNDUvMTM0MjgwMjU2NTUzMjYucG5nfDYyYzUyODI5MTA5NjM2ZWI5YjM1MDhmOWIwMjM2ODYxNDZiOTAzYjY3OGNmMGVlNWY4MjAxYjBjNzc3NzYzNDQ",
@@ -76,6 +92,8 @@ let data=[
       title:"Visibly lifts, sculpts + firms while minimizing the appearance of lines + deep wrinkles. ",
       name:"REDEFINE Regimen",
       price: 220,
+      rat:"3.9(3279)",
+      star:"https://cdn-icons-png.flaticon.com/128/992/992000.png",
       image1:"https://www.rodanandfields.com/en-us/medias/AAWA125-720x600-desktop-1.jpg?context=bWFzdGVyfGltYWdlc3wxOTg1Njh8aW1hZ2UvanBlZ3xpbWFnZXMvaDM3L2g5ZC8xNDc5ODgzMjU2NjMwMi5qcGd8OTAzYjY3NGU1YTUyNmNkYzcyZGM3NGEyMmQzYjIxY2ZiMTRjYjNjNzhmYWFiMzdkOGI0ODM1MjdiMTI0Nzk2Zg",
       image2:"https://www.rodanandfields.com/en-us/medias/AATN125-720x600-desktop-1.jpg?context=bWFzdGVyfGltYWdlc3wxOTk1ODd8aW1hZ2UvanBlZ3xpbWFnZXMvaGFkL2hmMi8xMTg1NDA1MzMxMDQ5NC5qcGd8Njg0OTgyMDU0NGI3MDhkNDY5OWZmMWI2YTYxMTg3OWZhODM5NTk3MTBjNTVlOTdiMTQ4YWE4ZmUzMWZkZDg4OA",
       image3:"https://www.rodanandfields.com/en-us/medias/CNVOUB2003-lifestyle-1168x1168-desktop-4.jpg?context=bWFzdGVyfGltYWdlc3wzMzA3NjF8aW1hZ2UvanBlZ3xpbWFnZXMvaGNkL2gxZi8xNDk5NjMxODMyMjcxOC5qcGd8NjY1YzIyNmNhMjQ3OGQ4YTAwNDljZDJiOGU5YTE4MDg3NGM0ZjU0YjVlMjE2ZmIyZWFiMzI1NDI5OGY3NDYyYQ",
@@ -85,6 +103,8 @@ let data=[
       title:"Visibly firms + lifts to improve the look of fine lines, wrinkles, sagging + crow’s feet. ",
       name:"REDEFINE Multi-Function Eye",
       price: 70,
+      rat:"4.7(359)",
+      star:"https://cdn-icons-png.flaticon.com/128/992/992001.png",
       image1:"https://i.ebayimg.com/images/g/8CoAAOSwNFphrcIq/s-l500.jpg",
       image2:"https://www.ubuy.co.in/productimg/?image=aHR0cHM6Ly9pLmViYXlpbWcuY29tL2ltYWdlcy9nLzg1d0FBT1N3YTJkaHFFalAvcy1sNTAwLmpwZw.jpg",
       image3:"https://www.rodanandfields.com/en-us/medias/AAEY015-lifestyle-1168x1168-desktop-4.jpg?context=bWFzdGVyfGltYWdlc3wxMjg1OTN8aW1hZ2UvanBlZ3xpbWFnZXMvaGZhL2gwMi8xMzUzMzEyMDk4NzE2Ni5qcGd8ZGQ2ODE0YWE1NDJiZjc2N2IxZjJiNTc4NGQwZmJmYzE2YzUyOWJkNzMzNzQwNzJjYzhjOWExOTg1NTA3ZTg1OQ",
@@ -94,6 +114,8 @@ let data=[
       title:"Multi-targeted skincare treatment that instantly and visibly firms, tightens + smooths.",
       name:"R+F Instant Targeted Firming Gel™",
       price: 105,
+      rat:"4.5(3368)",
+      star:"https://cdn-icons-png.flaticon.com/128/992/992001.png",
       image1:"https://www.rodanandfields.com/en-us/medias/ENFG010-ITFG-Lifesyle-570x570.jpg?context=bWFzdGVyfHJvb3R8NTc2NTl8aW1hZ2UvanBlZ3xoMGEvaGRmLzE0OTY5MDg2ODY5NTM0LmpwZ3wwYjA5YjRkN2YyNTZhZjRjYzhiODA1OGMyOGViMTlmZTg1N2E2NjM2MjI0NmUwYTk2YWZhMGI4ZmExZmY0ODA4",
       image2:"https://www.rodanandfields.com/en-us/medias/ENFGR10-ITFG-Refill-GIF-570x570.gif?context=bWFzdGVyfHJvb3R8NDAyNTkwfGltYWdlL2dpZnxoNDgvaDBhLzE0OTY5MDg3MzYxMDU0LmdpZnxjOGIyMmE5NDFiNzNkOTIxNWE4ZjM2MjRlMzRiYTc3ZjFiOWY4Y2NjODAwZDQzYWY4NWM0YTYxNWQ2YmI0Yzdh",
       image3:"https://www.rodanandfields.com/en-us/medias/ENFG010-B-A3-GLOBAL-455X455.jpg?context=bWFzdGVyfHJvb3R8NDAzMDl8aW1hZ2UvanBlZ3xoNmEvaDA3LzE0OTY5MDg2MjE0MTc0LmpwZ3w3NDg4ZGVjOTU4YmUyMGZmNmFkOTE5NGEzZmNjNjIwMDVjNDRhYjQwMTViMWQwOTk3ZDhiNGYwYmIyOTdmMzM0",
@@ -103,6 +125,8 @@ let data=[
       title:"For teens + young adults: Clears existing blemishes + prevents new ones from forming.",
       name:"SPOTLESS Regimen",
       price: 89,
+      rat:"4.7(5218)",
+      star:"https://cdn-icons-png.flaticon.com/128/992/992001.png",
       image1:"https://www.rodanandfields.com/en-us/medias/UNRS001-PDP-lifestyle-1168x1168-DESKTOP.jpg?context=bWFzdGVyfGltYWdlc3w2NjM0M3xpbWFnZS9qcGVnfGltYWdlcy9oMjAvaDcxLzg4MjE4Nzg0MjM1ODIuanBnfGUyYjI3MjFkYzQ3OTYyOGY0YzM3NGRlZDkwMzU4N2ZmNDBkMmYwOTcxOTdiNjAyMWViMGYzYjA1YTM1MzdkNDQ",
       image2:"https://www.rodanandfields.com/en-us/medias/UNRS001-B-A-PDP-455x455-Dakota.jpg?context=bWFzdGVyfGltYWdlc3w0NjA5MXxpbWFnZS9qcGVnfGltYWdlcy9oNWMvaDI2LzEzNjE2MDE5NTcwNzE4LmpwZ3xiZDVmMzBjN2QzZDkyNDgzYzAzNzY3ZjdkYTRlYWJkNzk3MWU2NWY2MTAzZDZiNzM5NWNmN2M0OTMyZjZhYTdm",
       image3:"https://www.rodanandfields.com/en-us/medias/UNRS001-detail.jpg?context=bWFzdGVyfGltYWdlc3w1NTAzMnxpbWFnZS9qcGVnfGltYWdlcy9oYzcvaGYxLzExMzU3NzEzMDA2NjIyLmpwZ3wyYTRiZjUzODkyOGQwZDljOWEyY2UyNWFiMDBjNjZmZjAyOGU4NzQwNzU3ODdjOTM4N2ZiOWJiNmIyNjgwYjQw",
@@ -116,6 +140,7 @@ let container=document.getElementById("container")
     container.innerHTML=null;
     data.forEach(function (el) {
     let div = document.createElement("div");
+    let ratdiv = document.createElement("div");
     let image_url = document.createElement("img");
     image_url.setAttribute("src", el.image);
     image_url.addEventListener("click",function(){
@@ -136,8 +161,13 @@ let container=document.getElementById("container")
     price.innerText="$"+el.price;
     let t=document.createElement("h6")
     t.innerText="Best Seller";
+    let star=document.createElement("img")
+    star.src=el.star;
+    let rat=document.createElement("h5")
+    rat.innerText=el.rat;
+    ratdiv.append(star,rat)
 
-    div.append(image_url,t, proName, dataPrice,price,btn);
+    div.append(image_url,t, proName, dataPrice,price,ratdiv,btn);
 
     container.append(div);
   });
