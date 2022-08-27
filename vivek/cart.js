9c348643ee081aa64f31dfdc1cb0c2f896d3cc0c
Vivek Maurya <vivekmauryarkt1998@gmail.com>
2022-08-27 16:20:11 +0530

    vivekday5

--- vivek/cart.js
+++ vivek/cart.js
@@ -1,34 +1,25 @@
-
-let container=document.getElementById("container")
-let cart=JSON.parse(localStorage.getItem("cart")) || [];
-function Display(data){
-  container.innerHTML=null;
-  data.forEach(function (el) {
-  let div = document.createElement("div");
-  div.addEventListener("click",function(){
-    show(el);
+let cartLS = JSON.parse(localStorage.getItem("cart")) || [];
+let sum=0;
+console.log(cartLS);
+let Display= (cartLS)=>{
+  let container= document.getElementById("container");
+  cartLS.forEach(function (ele){
+    let div=document.createElement("div");
+    let img=document.createElement("img");
+    img.src=ele.image;
+    let name=document.createElement("h2");
+    name.innerText=ele.name;
+    let price=document.createElement("h4");
+    price.innerText=ele.price;
+    let h1=document.getElementById("total");
+    sum+=parseInt(ele.price);
+    h1.innerText="Total Price:- "+sum;
+    div.append(img,name,price);
+    container.append(div);
   })
-  let image_url = document.createElement("img");
-  image_url.setAttribute("src", el.image);
-
-  let btn = document.createElement("button");
-  btn.innerText = "ADD TO BAG";
-  btn.addEventListener("click",function(){
-    addToCart(el);
-
-  })
-  let proName = document.createElement("h2");
-  proName.innerText = el.name;
-  let dataPrice = document.createElement("p");
-  dataPrice.innerText =el.title;
-  let price=document.createElement("h1")
-  price.innerText="$"+el.price;
-  let t=document.createElement("h6")
-  t.innerText="Best Seller";
-
-  div.append(image_url,t, proName, dataPrice,price,btn);
-
-  container.append(div);
-});
 }
-Display(cart);
+Display(cartLS);
+  
+
+
+
